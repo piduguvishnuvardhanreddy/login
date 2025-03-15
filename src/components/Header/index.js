985c7d4226ebea7eb52b179d056a57fcd61e3aa6
piduguvishnuvardhanreddy <pidugu.vishnu2@gmail.com>
2025-03-15 14:23:10 +0000

    Solution

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,78 +11,78 @@ const Header = props => {
   }
 
   return (
-    <nav className="nav-header">
-      <div className="nav-content">
-        <div className="nav-bar-mobile-logo-container">
-          <Link to="/">
+    <nav className='nav-header'>
+      <div className='nav-content'>
+        <div className='nav-bar-mobile-logo-container'>
+          <Link to='/'>
             <img
-              className="website-logo"
-              src="https://media.istockphoto.com/id/1345681613/vector/creative-people-logo-vector-illustration-design-editable-resizable-eps-10.jpg?s=612x612&w=0&k=20&c=9XUHICA1ljbxBcLw8ERp0kDDxLNQ8Bp2yR4aUSS6SBs="
-              alt="website logo"
+              className='website-logo'
+              src='https://media.istockphoto.com/id/1345681613/vector/creative-people-logo-vector-illustration-design-editable-resizable-eps-10.jpg?s=612x612&w=0&k=20&c=9XUHICA1ljbxBcLw8ERp0kDDxLNQ8Bp2yR4aUSS6SBs='
+              alt='website logo'
             />
           </Link>
 
           <button
-            type="button"
-            className="nav-mobile-btn"
+            type='button'
+            className='nav-mobile-btn'
             onClick={onClickLogout}
           >
             <img
-              src="https://media.istockphoto.com/id/1345681613/vector/creative-people-logo-vector-illustration-design-editable-resizable-eps-10.jpg?s=612x612&w=0&k=20&c=9XUHICA1ljbxBcLw8ERp0kDDxLNQ8Bp2yR4aUSS6SBs="
-              alt="nav logout"
-              className="nav-bar-img"
+              src='https://media.istockphoto.com/id/1345681613/vector/creative-people-logo-vector-illustration-design-editable-resizable-eps-10.jpg?s=612x612&w=0&k=20&c=9XUHICA1ljbxBcLw8ERp0kDDxLNQ8Bp2yR4aUSS6SBs='
+              alt='nav logout'
+              className='nav-bar-img'
             />
           </button>
         </div>
 
-        <div className="nav-bar-large-container">
-          <div className="nav-container-logo">
-            <div className="logo-text-container">
-              <Link to="/">
+        <div className='nav-bar-large-container'>
+          <div className='nav-container-logo'>
+            <div className='logo-text-container'>
+              <Link to='/'>
                 <img
-                  className="website-logo"
-                  src="https://media.istockphoto.com/id/1345681613/vector/creative-people-logo-vector-illustration-design-editable-resizable-eps-10.jpg?s=612x612&w=0&k=20&c=9XUHICA1ljbxBcLw8ERp0kDDxLNQ8Bp2yR4aUSS6SBs="
-                  alt="website logo"
+                  className='website-logo'
+                  src='https://media.istockphoto.com/id/1345681613/vector/creative-people-logo-vector-illustration-design-editable-resizable-eps-10.jpg?s=612x612&w=0&k=20&c=9XUHICA1ljbxBcLw8ERp0kDDxLNQ8Bp2yR4aUSS6SBs='
+                  alt='website logo'
                 />
               </Link>
-              <h1 className="text-logo nav-link">LOGO</h1>
+              <h1 className='text-logo nav-link'>LOGO</h1>
               <button
-                type="button"
-                className="logout-desktop-btn"
+                type='button'
+                className='logout-desktop-btn'
                 onClick={onClickLogout}
               >
                 Logout
               </button>
             </div>
           </div>
-          <div className="section-container">
-            <ul className="nav-menu">
-              <li className="nav-menu-item">
-                <Link to="/" className="nav-link">
+          <div className='section-container'>
+            <ul className='nav-menu'>
+              <li className='nav-menu-item'>
+                <Link to='/' className='nav-link'>
                   SHOP
                 </Link>
               </li>
 
-              <li className="nav-menu-item">
-                <Link to="/products" className="nav-link">
+              <li className='nav-menu-item'>
+                <Link to='/products' className='nav-link'>
                   SKILLS
                 </Link>
               </li>
 
-              <li className="nav-menu-item">
-                <Link to="/cart" className="nav-link">
+              <li className='nav-menu-item'>
+                <Link to='/cart' className='nav-link'>
                   STORIES
                 </Link>
               </li>
 
-              <li className="nav-menu-item">
-                <Link to="/cart" className="nav-link">
+              <li className='nav-menu-item'>
+                <Link to='/cart' className='nav-link'>
                   ABOUT
                 </Link>
               </li>
 
-              <li className="nav-menu-item">
-                <Link to="/cart" className="nav-link">
+              <li className='nav-menu-item'>
+                <Link to='/cart' className='nav-link'>
                   CONTACT US
                 </Link>
               </li>
